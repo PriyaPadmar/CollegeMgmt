d968de1f8b0999520522b3707cf0d7816563ee96
PriyaPadmar <118734007+PriyaPadmar@users.noreply.github.com>
2022-11-25 16:37:56 +0530

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'migrationdb.cgw8nrfaxzdz.eu-west-2.rds.amazonaws.com',
+	'username' => 'admin',
+	'password' => 'admin123',
+	'database' => 'college_mgmt',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
